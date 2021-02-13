1fa4df5ffc0b6d1324b355a550271bf0fcbbadc7
manasrajowar66 <58780138+manasrajowar66@users.noreply.github.com>
2021-02-13 13:46:25 +0530

    init

---
+++ Number Theory/PrimalityTest.cpp
@@ -0,0 +1,50 @@
+// O(sqrt(n))
+
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define lli long long int
+#define ll long long
+#define vec vector<int>
+#define for0() for (int i = 0; i < n; i++)
+#define for1() for (int i = 1; i <= n; i++)
+#define backloop() for (int i = n - 1; i >= 0; i--)
+#define tci() \
+    int t;    \
+    cin >> t; \
+    while (t--)
+#define endl "\n"
+#define OJ                            \
+    freopen("input.txt", "r", stdin); \
+    freopen("output.txt", "w", stdout);
+#define FIO                           \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+
+bool primalityTest(int n)
+{
+    for (int i = 2; i <= sqrt(n); i++)
+    {
+        if (n % i == 0)
+            return false;
+        else if (n % (n / i) == 0)
+            return false;
+    }
+    return true;
+}
+
+signed main()
+{
+    int n;
+    cin >> n;
+    if (primalityTest(n))
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+    return 0;
+}
