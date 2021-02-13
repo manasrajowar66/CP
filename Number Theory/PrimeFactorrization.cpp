d6da1c0c04cf79809bae406b60bf66a82aaaca68
manasrajowar66 <58780138+manasrajowar66@users.noreply.github.com>
2021-02-13 19:55:43 +0530

    init

---
+++ Number Theory/PrimeFactorrization.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define lli long long int
+#define ll long long
+#define vec vector<int>
+#define for0() for (int i = 0; i < n; i++)
+#define for1() for (int i = 1; i <= n; i++)
+#define backloop() for (int i = n - 1; i >= 0; i--)
+#define tci() \
+    int t;    \
+    cin >> t; \
+    while (t--)
+#define endl "\n"
+#define OJ                            \
+    freopen("input.txt", "r", stdin); \
+    freopen("output.txt", "w", stdout);
+#define FIO                           \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+
+void primeFactorization(int n)
+{
+    for (int i = 2; i <= sqrt(n); i++)
+    {
+        if (n % i == 0)
+        {
+            int cnt = 0;
+            while (n % i == 0)
+            {
+                n /= i;
+                cnt++;
+            }
+            cout << i << " " << cnt << endl;
+        }
+    }
+    if (n > 1)
+        cout << n << " " << 1 << endl;
+}
+
+signed main()
+{
+    int n;
+    cin >> n;
+    primeFactorization(n);
+    return 0;
+}
