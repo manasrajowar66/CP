e0442b39d3acf46c66f0f2c041af908b8de56620
manasrajowar66 <58780138+manasrajowar66@users.noreply.github.com>
2021-02-13 13:59:45 +0530

    init

--- Number Theory/PrimalityTest.cpp
+++ Number Theory/PrimalityTest.cpp
@@ -24,13 +24,11 @@ using namespace std;
 
 bool primalityTest(int n)
 {
-    for (int i = 2; i <= sqrt(n); i++)
+    for (int i = 2; i * i <= n; i++)
     {
         if (n % i == 0)
             return false;
-        else if (n % (n / i) == 0)
-            return false;
-    }
+        }
     return true;
 }
 
